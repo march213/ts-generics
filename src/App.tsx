196e24ae44cfbb52b68a4a7a9a320781c9e44a60
Jane Molodetskaya <jane.molodetskaya@gmail.com>
2022-02-23 23:38:48 -0500

    add input component; implement basic search

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import { SearchInput } from './components/SearchInput';
 import { IWidget } from './interfaces/IWidget';
 import { people } from './mock-data/people';
 import { widgets } from './mock-data/widgets';
 import { genericSearch } from './utils/genericSearch';
 
 function App() {
-  const query = ''
+  const [searchQuery, setSearchQuery] = useState<string>('')
 
   return (
     <div className="min-h-full">
       <main className="py-8">
-        <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-          <div>
+        <div className="max-w-sm my-4 px-4 sm:px-6 lg:px-8">
+          <SearchInput setSearchQuery={setSearchQuery} />
+        </div>
+        <div className="flex flex-col sm:flex-row max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+          <div className="min-w-[50%]">
             <h2 className="text-3xl font-bold mb-6">Widgets:</h2>
             {widgets
-              .filter((widget) => genericSearch<IWidget>(widget, ['title', 'description'], query))
+              .filter((widget) => genericSearch<IWidget>(widget, ['title', 'description'], searchQuery))
               .map((widget) => (
                 <div key={widget.id}>{widget.title}</div>
               ))}
           </div>
-          <div className="mt-12">
+          <div className="min-w-[50%]">
             <h2 className="text-3xl font-bold mb-6">People:</h2>
             {people
-              .filter((person) => genericSearch(person, ['firstName', 'lastName', 'eyeColor'], query))
+              .filter((person) => genericSearch(person, ['firstName', 'lastName', 'eyeColor'], searchQuery))
               .map((person) => (
                 <div key={person.lastName}>
                   {person.firstName} {person.lastName}
